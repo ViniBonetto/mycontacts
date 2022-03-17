52867fca980c2cde8a618b38ad641ad7526a5c3a
vinicius.bonetto <vinicius.bonetto@slcagricola.com.br>
2022-03-17 20:15:37 -0300

    Ajustes nos controllers

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -19,12 +19,52 @@ class ContactController {
     response.json(contact);
   }
 
-  store() {
+  async store(request, response) {
+    const {
+      name, email, phone, categoryId,
+    } = request.body;
 
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required!' });
+    }
+
+    const contactExists = await ContactRepository.findByEmail(email);
+    if (contactExists) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+    const contact = await ContactRepository.create({
+      name, email, phone, categoryId,
+    });
+
+    response.json(contact);
   }
 
-  update() {
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      name, email, phone, categoryId,
+    } = request.body;
 
+    const contactExists = await ContactRepository.findById(id);
+    if (!contactExists) {
+      return response.status(404).json({ error: 'User not found.' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required!' });
+    }
+
+    const contactByEmail = await ContactRepository.findByEmail(email);
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+    const contact = await ContactRepository.update(id, {
+      name, email, phone, categoryId,
+    });
+
+    response.json(contact);
   }
 
   async delete(request, response) {
